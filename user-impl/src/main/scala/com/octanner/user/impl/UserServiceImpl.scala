1710364ad9aa8c350a6f78e6dcb1ea9b71093905
apismensky <alexey.pismenskiy@octanner.com>
2016-05-19 14:01:01 -0600

    simplify

--- user-impl/src/main/scala/com/octanner/user/impl/UserServiceImpl.scala
+++ user-impl/src/main/scala/com/octanner/user/impl/UserServiceImpl.scala
@@ -5,7 +5,7 @@ import com.google.inject.Inject
 import com.lightbend.lagom.javadsl.api.ServiceCall
 import com.lightbend.lagom.javadsl.api.transport.NotFound
 import com.lightbend.lagom.javadsl.persistence.PersistentEntityRegistry
-import com.lightbend.lagom.javadsl.persistence.cassandra.{CassandraReadSide, CassandraSession}
+import com.lightbend.lagom.javadsl.persistence.cassandra.CassandraSession
 import com.octanner.image.api.ImageService
 import com.octanner.user.api.{User, UserService}
 import converter.ServiceCallConverter._
@@ -22,9 +22,13 @@ class UserServiceImpl @Inject()(imageService: ImageService,
 
   override def getUser(id: Long): ServiceCall[NotUsed, User] = { request =>
     println(s"Calling GET /api/users/$id")
-    imageService.getImage(id).invoke() flatMap { im =>
-      userEntityRef(id).ask[GetUserReply, GetUser](GetUser())
-        .map {_.user.getOrElse(throw new NotFound(s"user $id not found")).copy(image = Some(im))}
+    val futureImage = imageService.getImage(id).invoke()
+    futureImage flatMap { image =>
+      val futureUserReply = userEntityRef(id).ask[GetUserReply, GetUser](GetUser())
+      futureUserReply map { userReply =>
+        val user: User = userReply.user.getOrElse(throw new NotFound(s"user $id not found"))
+        user.copy(image = Some(image))
+      }
     }
   }
 
